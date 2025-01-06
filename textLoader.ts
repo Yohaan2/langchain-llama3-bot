515f9bb21c99dec7097e24d3d0f3c227df6dc0f1
yohaan2 <garciayohan57@gmail.com>
2025-01-06 16:58:34 -0400

    Refactor textLoader to streamline prompt handling and remove unused imports

--- textLoader.ts
+++ textLoader.ts
@@ -1,13 +1,12 @@
 import { TextLoader } from 'langchain/document_loaders/fs/text'
-import { JSONLoader, JSONLinesLoader } from 'langchain/document_loaders/fs/json'
+import { JSONLoader } from 'langchain/document_loaders/fs/json'
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf'
 import { Ollama } from '@langchain/community/llms/ollama'
 import { OllamaEmbeddings } from '@langchain/community/embeddings/ollama'
 import { Chroma } from '@langchain/community/vectorstores/chroma'
-import { pull } from 'langchain/hub'
 import { createStuffDocumentsChain } from 'langchain/chains/combine_documents'
 import { StringOutputParser } from '@langchain/core/output_parsers'
-import { PromptTemplate, BaseChatPromptTemplate } from '@langchain/core/prompts'
+import { PromptTemplate } from '@langchain/core/prompts'
 
 const loaders = {
 	txt: (filePath: string) => new TextLoader(filePath),
@@ -47,24 +46,21 @@ export const textLoader = async (question: string, path: string) => {
 	const docs = await getLoader(path)
 	await vectorStoreChroma.addDocuments(docs)
 	const retrievedDocs = await vectorStoreChroma.similaritySearch(question)
-	console.log(docs.map((doc) => doc.pageContent))
+	//console.log(docs.map((doc) => doc.pageContent))
 
-	// pull the rag prompt from langsmith
-	const prompt = (await pull('rlm/rag-prompt')) as any
-
-	const personalicedPrompt =
-		PromptTemplate.fromTemplate(`Eres un asistente para tareas de respuesta a preguntas. Usa los siguientes fragmentos de contexto recuperados para responder la pregunta. Si no sabes la respuesta, simplemente di que no la sabes. Usa tres oraciones como máximo y mantén la respuesta concisa.
+	const personalizedPrompt =
+		PromptTemplate.fromTemplate(`Eres un asistente para tareas de respuesta a preguntas. Usa los siguientes fragmentos de contexto recuperados para responder la pregunta. Si no sabes la respuesta, simplemente di que no la sabes
   Pregunta: {question}
   Contexto: {context}
   Respuesta:`)
 	// to convert the model output to standardized string
-	const ragChain = await createStuffDocumentsChain({
+	const chain = await createStuffDocumentsChain({
 		llm,
-		prompt: personalicedPrompt,
+		prompt: personalizedPrompt,
 		outputParser: new StringOutputParser(),
 	})
 
 	// pass the question and the relevant docs to the LLM
-	const result = await ragChain.invoke({ question, context: retrievedDocs })
-	console.log(result) // Optimus will cost around $20-$30K, which is less than a car.
+	const result = await chain.invoke({ question, context: retrievedDocs })
+	console.log(result)
 }
